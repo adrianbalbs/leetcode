3183cd06793951190390de24cc3c670442d1257d
adrianbalbs <adrianbalbs@hotmail.com>
2023-03-30 10:04:34 +1100

    Finished solution to 121

--- 121-best-time-to-buy-and-sell-stock/121.py
+++ 121-best-time-to-buy-and-sell-stock/121.py
@@ -1,13 +1,12 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-            i,j = 0,1
-            maxTotal = 0
-            while j != len(prices):
-                if prices[i] > prices[j]:
-                    i += 1
-                    j += 1
-                else:
-                    maxTotal = max(maxTotal, prices[j] - prices[i])
-                    j += 1
-            return maxTotal
-
+        i, j = 0, 1
+        maxTotal = 0
+        while j < len(prices):
+            if prices[i] < prices[j]:
+                profit = prices[j] - prices[i]
+                maxTotal = max(maxTotal, profit)
+            else:
+                i = j
+            j += 1
+        return maxTotal
