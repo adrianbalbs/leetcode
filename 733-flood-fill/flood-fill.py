79fc48beb2ea4290f9e498f62f1651bb408b219f
adrianbalbs <adrianbalbs@hotmail.com>
2023-08-07 23:10:31 +1000

    733 Flood Fill

---
+++ 733-flood-fill/flood-fill.py
@@ -0,0 +1,28 @@
+class Solution:
+    def floodFill(self, image: list[list[int]], sr: int, sc: int, color: int) -> list[list[int]]:
+        visited = [ [False] * len(image[i]) for i in range(len(image))]
+        print(visited)
+        queue = []
+        queue.append((sr, sc))
+        while queue:
+            i, j = queue.pop()
+
+            if i - 1 >= 0 and not visited[i - 1][j] and image[i - 1][j] == image[i][j]:
+                queue.append((i - 1, j))
+
+            if i + 1 <= len(image) - 1 and not visited[i + 1][j] and image[i + 1][j] == image[i][j]:
+                queue.append((i + 1, j))
+
+            if  j - 1 >= 0 and not visited[i][j - 1] and image[i][j - 1] == image[i][j]:
+                queue.append((i, j - 1))
+
+            if j + 1 <= len(image[i]) - 1 and not visited[i][j + 1] and image[i][j + 1] == image[i][j]:
+                queue.append((i, j + 1))
+
+            image[i][j] = color
+            visited[i][j] = True
+
+        return image
+
+sol = Solution()
+print(sol.floodFill([[1,1,1],[1,1,0],[1,0,1]], 1, 1, 2))
