6bdaabe68acf8b5f2073351cbbe929d394846e9e
adrianbalbs <adrianbalbs@hotmail.com>
2023-08-22 10:26:49 +1000

    110 - Balanced Binary Tree

---
+++ 110-balanced-binary-tree/balanced_binary_tree.py
@@ -0,0 +1,26 @@
+from typing import Optional
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Solution:
+    def isBalanced(self, root: Optional[TreeNode]) -> bool:
+        _, IsBalanced = self.isBalancedHelper(root)
+        return IsBalanced
+
+    def isBalancedHelper(self, root) -> tuple[int, bool]:
+        if not root:
+            return (-1, True)
+        lHeight, lIsBalanced = self.isBalancedHelper(root.left)
+        rHeight, rIsBalanced = self.isBalancedHelper(root.right)
+
+        balance = abs(lHeight - rHeight)
+        if balance <= 1 and lIsBalanced and rIsBalanced:
+            height = max(lHeight, rHeight) + 1
+            return (height, True)
+        else:
+            return (-1, False)
---
+++ 110-balanced-binary-tree/notes.md
@@ -0,0 +1,9 @@
+# Notes
+
+- Time Elapsed: 15 Minutes
+
+### Solution
+
+- Return the height and boolean in a tuple
+- Calculate in post-order traversal to go from the bottom up
+- If we have any false booleans, just bubble the recursion back up
