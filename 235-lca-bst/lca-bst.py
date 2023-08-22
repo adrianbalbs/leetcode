095497bccdc452345997173c30a14856d0d554ce
adrianbalbs <adrianbalbs@hotmail.com>
2023-08-23 09:34:45 +1000

    235 Lowest common ancestor of a BST

--- 235-lca-bst/lca-bst.py
+++ 235-lca-bst/lca-bst.py
@@ -7,12 +7,30 @@ class TreeNode:
 
 class Solution:
     def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
-        return root
-    def binarySearch(self, root: 'TreeNode', val: 'TreeNode') -> 'TreeNode':
-        if root is None or root.val == val:
+        # Store the paths to both p and q in two lists, obtain it through binary search 
+        pathP: list['TreeNode'] = []
+        pathQ: list['TreeNode'] = []
+        self.binarySearch(root, p, pathP)
+        self.binarySearch(root, q, pathQ)
+
+        res = p
+        for nodeP, nodeQ in zip(pathP, pathQ):
+            if nodeP != nodeQ:
+                break
+            res = nodeP
+
+        return res 
+
+    def binarySearch(self, root: 'TreeNode', node: 'TreeNode', pathList: list['TreeNode']) -> 'TreeNode':
+        if root is None:
+            return root
+        elif root.val == node.val:
+            pathList.append(root)
             return root
 
-        if root.val < val:
-            return self.binarySearch(root.right, val)
+        if root.val < node.val:
+            pathList.append(root)
+            return self.binarySearch(root.right, node, pathList)
 
-        return self.binarySearch(root.left, val)
+        pathList.append(root)
+        return self.binarySearch(root.left, node, pathList)
---
+++ 235-lca-bst/notes.md
@@ -0,0 +1,12 @@
+# Notes
+
+Time Elapsed: 20 Mins
+
+### Solution
+
+- Store the paths to p and q in two arrays respectively
+- Loop through both arrays until they do not match
+
+### Optimal Solution
+
+- Modified binary search with checking the condition for both values instead of just 1
